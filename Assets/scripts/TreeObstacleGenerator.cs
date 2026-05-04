2df67be9750cb53e4f51c282c362b842a5253663
JusticeHurt <justicehurt7@icloud.com>
2026-05-04 18:57:32 -0500

    NavMesh fix for painted trees.
    Using script now to do the nav mesh so deer and monsters dont phase through anymore.

---
+++ Assets/scripts/TreeObstacleGenerator.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TreeObstacleGenerator : MonoBehaviour
+{
+    void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        TerrainData data = terrain.terrainData;
+
+        foreach (TreeInstance tree in data.treeInstances)
+        {
+            // position of the tree
+            Vector3 worldPos = Vector3.Scale(tree.position, data.size) + terrain.transform.position;
+
+            //temporary GameObject to hold the obstacle
+            GameObject obstacleFolder = new GameObject("TreeObstacle");
+            obstacleFolder.transform.position = worldPos;
+            obstacleFolder.transform.parent = this.transform;
+
+            // Add the NavMesh Obstacle component
+            NavMeshObstacle nmo = obstacleFolder.AddComponent<NavMeshObstacle>();
+            nmo.shape = NavMeshObstacleShape.Capsule;
+            nmo.radius = 2f;
+            nmo.height = 4f;
+            nmo.carving = true; 
+        }
+    }
+}
